efa76ef53864add1037f7e60b0b6a7400aa715cd
Steffen Klee <steffen.klee@gmail.com>
2022-05-31 22:08:05 +0200

    Version 0.4.9

--- samsungScannerServer.py
+++ samsungScannerServer.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.4.5"
+__version__ = "0.4.9"
 
 import atexit
 import datetime
